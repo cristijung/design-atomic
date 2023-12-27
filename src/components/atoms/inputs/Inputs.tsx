3fd9892970ff716348352fca34625bd1cde542d9
Cristina Jung <cristijung@gmail.com>
2023-12-27 14:48:26 -0300

    passando as props simples entre átomos e moléculas

--- src/components/atoms/inputs/Inputs.tsx
+++ src/components/atoms/inputs/Inputs.tsx
@@ -1,9 +1,16 @@
-//import styles from './Inputs.module.scss'
+import { ChangeEvent } from "react";
+//import styles from './Inputs.module.scss';
 
-export default function Inputs() {
-    return(
-        <>
-        
-        </>
-    )
+interface InputProps {
+  value: string | number; //recebendo string ou número
+  onChange: (value: string | number) => void;
 }
+
+function Inputs({ value, onChange }: InputProps) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+  return <input type="text" value={value} onChange={handleChange} />;
+}
+
+export default Inputs;
--- src/components/molecules/cards/Cards.tsx
+++ src/components/molecules/cards/Cards.tsx
@@ -1,12 +1,32 @@
 //import styles from './Cards.module.scss;'
-// import Buttons from "../../atoms/buttons/Buttons";
-// import Inputs from "../../atoms/inputs/Inputs";
+import { useState } from "react";
+import Buttons from "../../atoms/buttons/Buttons";
+import Inputs from "../../atoms/inputs/Inputs";
 // import Labels from "../../atoms/labels/Labels";
 
-export default function Cards() {
-    return(
-        <>
-        
-        </>
-    )
+//aqui vai o código de exemplo de uso dos componentes:
+//- Inputs e Buttons -- sem nada estilizado por enquanto
+
+interface CardsProps {}
+
+function Cards(props: CardsProps) {
+  const [inputValue, setInputValue] = useState<string | number>("");
+  const handleInputChange = (value: string | number) => {
+    setInputValue(value);
+  };
+  const handleButtonClick = () => {
+    console.log('Botão clicado');
+  }
+
+  return (
+    <>
+      <div>
+        <Inputs value={inputValue} onChange={handleInputChange} />
+        <Buttons onClick={handleButtonClick} label="Clique aqui" />
+        <p>Valor: {inputValue}</p>
+      </div>
+    </>
+  );
 }
+
+export default Cards;
